c34701f449e4339d4cc567c3f282eb061d5f9036
Prasad Kashid <108520855+PrasadKashid@users.noreply.github.com>
2024-07-11 23:43:25 +0530

    11th July Assignments
    Palindrome 
    CountSpace

---
+++ 11July/CountSpace.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountSpace
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a sentence ");
+            string inputString = Console.ReadLine();
+            Console.WriteLine(CountSpace(inputString));
+        }
+        static int CountSpace(string input)
+        {
+            int count = 0;
+            foreach (char a in input)
+            {
+                if (a == ' ')
+                    count++;
+            }
+            return count;
+        }
+    }
+
+}
---
+++ 11July/StringPalindrome.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringPalindrome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a String: ");
+            string inputString = Console.ReadLine();
+            inputString = inputString.ToLower();
+            Console.WriteLine(IsPalindrome(inputString));
+        }
+        static string IsPalindrome(string input)
+        {
+            char[] inputString = input.ToCharArray();
+            Array.Reverse(inputString);
+            string reverseString = new string(inputString);
+            return input == reverseString ? "It's a Palindrome" : "It's not a Palindrome";
+        }
+    }
+}
