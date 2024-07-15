4c0fe9ba8493e25fb241f70fb1f2dbd4ce607edf
Prasad Kashid <108520855+PrasadKashid@users.noreply.github.com>
2024-07-15 23:28:31 +0530

    13July Assignments
    Number Guessing game
    Play pig dice game

---
+++ 13July/NumberGuess.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberGuessing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int randomNum = RandomNumber(1, 20);
+
+            Console.WriteLine("Guess the number between 1 to 20: ");
+            int guessedNumber = int.Parse(Console.ReadLine());
+            int count = 1;
+            while(guessedNumber != randomNum)
+            {
+                if (randomNum > guessedNumber)
+                {
+                    Console.WriteLine("Enter a greater number: ");
+                }
+                else if(randomNum < guessedNumber)
+                {
+                    Console.WriteLine("Enter a Smaller number: ");
+                }
+                count++;
+                Console.WriteLine("Guess the number between 1 to 20: ");
+                guessedNumber = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine($"You've guessed correct number after {count} attempts");
+        }
+        public static int RandomNumber(int start, int end)
+        {
+            Random rnd = new Random();
+            int rndInt = rnd.Next(start, end);
+            return rndInt;
+        }
+    }
+}
