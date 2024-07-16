97632278cfda90d5eeb097378c4c9688e9fbf0aa
Prasad Kashid <108520855+PrasadKashid@users.noreply.github.com>
2024-07-16 22:27:27 +0530

    14th July assignments
    Product app

---
+++ 14July/Model.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ProductApp
+{
+    internal class Model
+    {
+        private int _productId;
+        private string _productName;
+        private double _productPrice;
+        private double _productDiscount;
+
+        public Model(int productId, string productName,double productPrice) : this(productId, productName, productPrice, 0) { }
+
+        public Model(int productId, string productName, double productPrice, double productDiscount)
+        {
+            _productId = productId;
+            _productName = productName;
+            _productPrice = productPrice;
+            _productDiscount = productDiscount;
+        }
+        public void SetDiscount(double applyDiscount)
+        {
+            _productDiscount = applyDiscount;
+        }
+
+        public static Model FindProduct(Model[] products, int productId) {
+            foreach(var product in products)
+            {
+                if(product._productId == productId)
+                {
+                    return product;
+                }
+            }
+            return null;
+        }
+
+        public static Model PrintDetails(Model[] products, int productId)
+        {
+            Model product = FindProduct(products, productId);
+            if(product!= null)
+            {
+                return product;
+            }
+            return null;
+        }
+
+        public override string ToString()
+        {
+            return "ProductId : " + _productId + " Productname : " + _productName + " Product Price " +_productPrice ;
+        }
+
+        public static double GetProductDiscount(Model[] products, int productId)
+        {
+            Model product = FindProduct(products, productId);
+            double discountAmount = (product._productDiscount / 100) * product._productPrice;
+            double finalDiscountPrice = product._productPrice - discountAmount;
+            return finalDiscountPrice;
+        }
+
+    }
+}
---
+++ 14July/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProductApp;
+
+namespace ProductApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Model[] products = new Model[]
+            {
+                new Model(101, "Bag", 1500),
+                new Model(102, "T shirt", 500),
+            };
+
+            Console.WriteLine("Enter a product id");
+            int productId = int.Parse(Console.ReadLine());
+            Model selectedProduct = (Model.PrintDetails(products, productId));
+            Console.WriteLine(selectedProduct);
+            Console.WriteLine();
+
+            Console.WriteLine("Enter a Product discount");
+            double discountPercentage = double.Parse(Console.ReadLine());
+            selectedProduct.SetDiscount(discountPercentage);
+
+
+            Console.WriteLine();
+            Console.WriteLine($"Product price after applying {discountPercentage}% discount : " + Model.GetProductDiscount(products, productId));
+
+
+        }
+    }
+}
